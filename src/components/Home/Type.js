0b8ea6878315ac12b8461da17720bd02379553b1
atq013 <attiqueurrehman013@gmail.com>
2024-08-28 01:32:12 +0500

    first

---
+++ src/components/Home/Type.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Software Development",
+          "AWS NGINX DEVOPS",
+          "NEST NEXT TYPESCRIPT JAVASCRIPT",
+          "MERN Stack Developement",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
